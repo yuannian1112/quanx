f60f5477899ac4a5b50ad70a3d3091c7a37db32f
xzxxn <2669371896>
2022-09-06 19:17:49 +0800

    1

---
+++ XmSport/XmSport_getToken.js
@@ -0,0 +1,15 @@
+const bodyName = '小米运动获取token'
+
+if ($response.body) {
+    const body = JSON.parse($response.body);
+    const loginToken = body.token_info.login_token;
+    $.log(`${$.name}token\n${loginToken}\n`)
+    if ($persistentStore.read('xmSportsToken')) {
+        $notification.post(bodyName, '更新Token: 成功🎉', ``);
+    } else {
+        $notification.post(bodyName, '获取Token: 成功🎉', '');
+    }
+    $persistentStore.write(loginToken, 'xmSportsToken');
+}
+
+$done({})
