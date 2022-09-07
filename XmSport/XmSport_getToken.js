f4b78314aeeecbfbbeb49e8d1a12d80ae8e2f3be
xzxxn <65740340@qq.com>
2022-09-07 14:31:38 +0800

    小米运动

--- XmSport/XmSport_getToken.js
+++ XmSport/XmSport_getToken.js
@@ -1,7 +1,7 @@
 const bodyName = '小米运动获取token'
 
 if ($response.body) {
-    const body = $response.body
+    const body = JSON.parse($response.body);
     const loginToken = body.token_info.login_token;
     if ($persistentStore.read('xmSportsToken')) {
         $notification.post(bodyName, '更新Token: 成功🎉', ``);
